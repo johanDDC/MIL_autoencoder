689b3ab2c89fb018444422f4a75d31580fb7bc34
ivan <ivan.peshekhonov@gmail.com>
2023-02-01 23:51:37 +0300

    Add blackwhite autoencoder

--- src/data/collators.py
+++ src/data/collators.py
@@ -6,3 +6,21 @@ def fc_collator(batch):
     features = torch.stack([torch.cat([features, elem[0]], dim=0) for elem in batch])
     targets = torch.tensor([elem[1] for elem in batch])
     return torch.flatten(features, start_dim=1), targets
+
+def pretrain_collator(batch):
+    features = torch.Tensor()
+    features = torch.stack([torch.cat([features, elem[0]], dim=0) for elem in batch])
+    return features, None
+
+def bw_collator(batch, no_targets=False):
+    features = [torch.cat([elem[0], torch.zeros_like(elem[0]), torch.zeros_like(elem[0])], dim=0) for elem in batch]
+    features = torch.stack(features)
+    targets = None
+    # features = torch.stack([features, torch.dstack([elem[0], torch.zeros_like(elem[0]), torch.zeros_like(elem[0])]) for elem in batch])
+    # print(features.shape)
+    if not no_targets:
+        if type(batch[0][1]) == int:
+            targets = torch.tensor([elem[1] for elem in batch])
+        else:
+            targets = torch.stack([elem[1] for elem in batch])
+    return features, targets
--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,18 +1,100 @@
+import glob
+
 import torchvision
 import torchvision.transforms as T
+import torchvision.transforms.functional as TF
+from PIL import Image
+from skimage import io
+from torch.utils.data import Dataset
+from torchvision.datasets import ImageFolder
+from tqdm import tqdm
+from src.utils.utils import Rotation
 
-transforms = T.Compose([
+color_common = T.Compose([
     T.ToTensor(),
     T.Normalize(
         (0.5, 0.5, 0.5),
         (0.5, 0.5, 0.5)
-    )
+    ),
+])
+
+bw_common = T.Compose([
+    T.ToTensor()
+])
+
+train_trainsform = lambda cmn: T.Compose([
+    T.RandAugment(9, 1),
+    cmn,
+    T.RandomErasing(p=0.25),
+    Rotation([0, 90, 180, 270])
+])
+
+tta = lambda cmn: T.Compose([
+    T.RandAugment(9, 1),
+    cmn
 ])
 
 
 def get_train_dataset(dataset_path="data", download=False):
-    return torchvision.datasets.CIFAR100(dataset_path, train=True, download=download, transform=transforms)
+    return torchvision.datasets.CIFAR100(dataset_path, train=True, download=download, transform=common)
 
 
 def get_val_dataset(dataset_path="data", download=False):
-    return torchvision.datasets.CIFAR100(dataset_path, train=False, download=download, transform=transforms)
+    return torchvision.datasets.CIFAR100(dataset_path, train=False, download=download, transform=common)
+
+
+class SSLDataset(Dataset):
+    def __init__(self, path, train=False, labled=False):
+        self.labled = labled
+        if train:
+            self.transforms = train_trainsform(color_common)
+        else:
+            self.transforms = tta(color_common)
+        if labled:
+            self.data = ImageFolder(path, transform=T.Compose([T.ToTensor()]))
+        else:
+            imgs = list(glob.iglob(f"{path}/*.jpg", recursive=True))
+            self.data = []
+            for img in tqdm(imgs, desc="Dataset loading"):
+                self.data.append(io.imread(img))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        img = self.data[item]
+        if self.labled:
+            img, target = img
+        else:
+            target = color_common(TF.to_pil_image(img.copy()))
+        img = self.transforms(TF.to_pil_image(img))
+        return img, target
+
+
+class BlackWhiteDataset(Dataset):
+    def __init__(self, path, train=False, labled=False):
+        self.labled = labled
+        grayscale = T.Grayscale(num_output_channels=1)
+        if train:
+            self.transforms = train_trainsform(bw_common)
+        else:
+            self.transforms = tta(bw_common)
+        if labled:
+            self.data = ImageFolder(path, transform=T.Compose([grayscale]))
+        else:
+            imgs = list(glob.iglob(f"{path}/*.jpg", recursive=True))
+            self.data = []
+            for img in tqdm(imgs, desc="Dataset loading"):
+                self.data.append(grayscale(Image.open(img)))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        img = self.data[item]
+        if self.labled:
+            img, target = img
+        else:
+            target = bw_common(img.copy())
+        img = self.transforms(img)
+        return img, target
